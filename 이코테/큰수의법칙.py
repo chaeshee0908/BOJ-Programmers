80c9cbb33af9c311c821782fcfdc834f35c4cc41
채승희 <chaeseunghui@chaeseunghee-MacBookPro.local>
2021-07-29 15:50:29 +0900

    7/29

--- 이코테/큰수의법칙.py
+++ 이코테/큰수의법칙.py
@@ -7,7 +7,7 @@ data.sort(reverse=True)     # 큰 수부터 정렬
 first = data[0]     # 가장 큰 수
 second = data[1]    # 두 번째로 큰 수
 
-s_time = M // K
+s_time = M // (K + 1)
 f_time = M - s_time
 
 result = first * f_time + second * s_time
