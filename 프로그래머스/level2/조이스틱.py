0d227915fba5bc2e07e5ea55ec997adad258abf0
chaeshee0908 <chaeshee0908@naver.com>
2021-08-06 15:55:26 +0900

    8/6

--- 프로그래머스/level2/조이스틱.py
+++ 프로그래머스/level2/조이스틱.py
@@ -1,5 +1,28 @@
 # 그리디 알고리즘
 
+# 다른이의 정답
+# def alphabet_to_num(char):
+#     num_char = [i for i in range(14)] + [j for j in range(12, 0, -1)]
+#     return num_char[ord(char) - ord('A')]
+
+# def solution(name):
+#     answer = 0
+#     n = len(name)
+
+#     for ch in name:
+#         answer += alphabet_to_num(ch)
+
+#     move = n - 1
+#     for idx in range(n):
+#         next_idx = idx + 1
+#         while (next_idx < n) and (name[next_idx] == 'A'):
+#             next_idx += 1
+#         distance = min(idx, n - next_idx) # 방향전환 해결_min
+#         move = min(move, idx + n - next_idx + distance)
+
+#     answer += move
+#     return answer
+
 def make_A(alphabet):
     diff = ord(alphabet) - ord('A')
     if diff > 13:
@@ -37,6 +60,8 @@ def solution(name):
 name1 = "JEROEN"
 name2 = "JAN"
 name3 = 'ABABAAAAAAABA'     # 10이 나와야함. 아마 한 방향만 고려해서 그런듯함
+name4 = 'BBABAAAB'
 print(solution(name1))
 print(solution(name2))
 print(solution(name3))
+print(solution(name4))
